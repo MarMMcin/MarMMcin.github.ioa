c6e87ea66f97ec75ffa697145c4c7a449c10c499
MarMMcin <em-erce@wp.pl>
2019-09-16 01:36:20 +0200

    Updates

---
+++ precache-manifest.fc4b576856260df0a52927eb0d81cf3e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d923c3b2c9c98c08b688cd7afcba02b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d138ef66f10e0e34f14c",
+    "url": "/static/css/main.3e0436ac.chunk.css"
+  },
+  {
+    "revision": "2963707322b234cacbb2",
+    "url": "/static/js/2.dd097d01.chunk.js"
+  },
+  {
+    "revision": "d138ef66f10e0e34f14c",
+    "url": "/static/js/main.33389813.chunk.js"
+  },
+  {
+    "revision": "a90ca305e43eaeb2a8e0",
+    "url": "/static/js/runtime~main.5a9f0d54.js"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/static/media/reactrouter.02419671.svg"
+  },
+  {
+    "revision": "3f5ee2d295271db65f0c48f17b34d619",
+    "url": "/static/media/rzeszow.3f5ee2d2.jpg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/static/media/sass.b446ca5a.svg"
+  }
+]);
